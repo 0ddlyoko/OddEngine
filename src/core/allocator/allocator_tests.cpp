bda303e585786edd74ffadd70efebfcc87da0851
0ddlyoko <11074473+0ddlyoko@users.noreply.github.com>
2023-12-23 01:41:27 +0100

    Fix tests

--- src/core/allocator/allocator_tests.cpp
+++ src/core/allocator/allocator_tests.cpp
@@ -7,9 +7,3 @@ TEST(HelloTest, BasicAssertions) {
     EXPECT_EQ(7 * 6, 42);
     EXPECT_EQ(7 * 6, 42);
 }
-
-int main(int argc, char **argv)
-{
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
